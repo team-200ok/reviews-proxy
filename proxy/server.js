ca917fb4f1ce91ab9b413b7b20c151e081e372d5
DanFein <danfeinstat@gmail.com>
2020-01-09 18:55:57 -0800

    udpated for aws deployment

--- proxy/server.js
+++ proxy/server.js
@@ -10,30 +10,7 @@ const port = 3000;
 app.use('/:id', express.static(path.join(__dirname, 'public')));
 app.use(
     '/sdc',
-    proxy({ target: 'http://localhost:3001', changeOrigin: true })
+    proxy({ target: 'http://ec2-54-183-172-215.us-west-1.compute.amazonaws.com:3001', changeOrigin: true })
 );
-// app.get('/business/:id', async function (req, res) {
-//     const response = await rp(`http://localhost:3001/sdc/business/${req.params.id}`)
-//         .catch((err) => {
-//             console.error(err.stack);
-//             res.status(500).end();
-//         });
-//     res.status(200).send(response).end();
-// })
 
-// app.get('/business/:id', function (req, res) {
-//     http.get(`http://localhost:3001/sdc/business/${req.params.id}`, (response) => {
-//         let data = '';
-
-//         response.on('data', (chunk) => {
-//             data += chunk;
-//         });
-
-//         response.on('end', () => {
-//             res.status(200).send(data).end();
-//         });
-//     }).on('error', (err) => {
-//         console.error(err.stack);
-//     })
-// })
 app.listen(port, () => console.log('Serving up fresh reviews on port', port));
